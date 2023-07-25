8fb1dcc270856b48b6206a8808719aea18419337
Matthew Neissen <106018830+hifive1278@users.noreply.github.com>
2023-07-25 16:18:36 -0400

    Update testmodel.py

--- testmodel.py
+++ testmodel.py
@@ -1,22 +1,3 @@
-# from sentence_transformers import SentenceTransformer
-# model = SentenceTransformer('all-MiniLM-L6-v2')
-
-# #Our sentences we like to encode
-# sentences = ['This framework generates embeddings for each input sentence',
-#     'Sentences are passed as a list of string.',
-#     'The quick brown fox jumps over the lazy dog.']
-
-# #Sentences are encoded by calling model.encode()
-# embeddings = model.encode(sentences)
-
-# #Print the embeddings
-# for sentence, embedding in zip(sentences, embeddings):
-#     print("Sentence:", sentence)
-#     print("Embedding:", embedding)
-#     print("")
-
-# ----------------------------------------------------------------
-
 from sentence_transformers import SentenceTransformer, util
 from torch import max, mean
 model = SentenceTransformer('all-MiniLM-L6-v2')
